b4f9c995d7e9f81ae28c12e67678ec217fe3a823
Alexis Tonneau <atonneau@juniorisep.com>
2021-10-28 16:26:52 -0700

    Prevent from spam

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 
 from twilio.rest import Client
+import pickle
 
 account_sid = os.getenv('ACCOUNT_SID')
 auth_token = os.getenv('AUTH_TOKEN')
@@ -17,6 +18,25 @@ stores_here = ["R057", "R414", "R075"]
 def send_message(text: str):
     msg = client.messages.create(to=my_phone_number, from_=phone_number, body=text)
     print(msg.sid)
+
+
+def check_current_stores(stores):
+    output = []
+    with open('available_stores.txt', 'rb') as f:
+        try:
+            available_stores = pickle.load(f)
+        except EOFError:
+            available_stores = []
+    for store in stores:
+        if store not in available_stores:
+            output.append(store)
+    return output
+
+
+def rewrite_current_stores(stores):
+    open('available_stores.txt', 'w').close()
+    with open('available_stores.txt', 'wb') as f:
+        pickle.dump(stores, f)
 
 
 def parse_request():
@@ -35,11 +55,12 @@ def parse_request():
         if store['storeNumber'] in stores_here and store['partsAvailability']['MLN13LL/A'][
             'pickupDisplay'] == 'available':
             available_stores.append(store['address']['address'])
-    print(available_stores)
-    if len(available_stores) > 0:
+    checked_stores = check_current_stores(available_stores)
+    if len(checked_stores) > 0:
         send_message("iPhone 13 dispo ! \n" + "\n".join(available_stores))
+        rewrite_current_stores(available_stores)
         return
-    print('Not available')
+    print('Not available' if len(available_stores) == 0 else f"Not available but {','.join(available_stores)}")
 
 
 if __name__ == '__main__':
